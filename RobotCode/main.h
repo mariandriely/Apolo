9d1243161bba7c0988f092f42708d50bf70ecab3
Gilberth <dev.gilberth@gmail.com>
2018-09-11 14:28:33 -0300

    código atualizado

---
+++ RobotCode/main.h
@@ -0,0 +1,15 @@
+
+float valorCorte[4] = {0, 0, 0, 0};
+float valorPreto[4] = {0, 0, 0, 0};
+float valorBranco[4] = {0, 0, 0, 0};
+
+bool btnCalibrarPreto;
+bool btnCalibrarBranco;
+bool btnSeguirLinha;
+bool seguirLinha = false;
+bool rampa = false;
+
+void calibrarBranco();
+void calibrarPreto();
+void initRampa();
+void initSeguirLinha();
